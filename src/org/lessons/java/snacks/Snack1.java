443c8ecd38f95306d4a7728554cd2d892930c26f
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-14 11:40:07 +0200

    Snack 1 completed

--- src/org/lessons/java/snacks/Snack1.java
+++ src/org/lessons/java/snacks/Snack1.java
@@ -1,10 +1,25 @@
 package org.lessons.java.snacks;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		int num = sc.nextInt();
+		sc.close();
+		boolean pair = num % 2 == 0;
+		
+		if (!pair) {
+			num++;
+		}
+		
+		System.out.println("Numero risultante: " + num);
+		
 	}
 
 }
