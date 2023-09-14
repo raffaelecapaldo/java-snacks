3fb92b0290cd9e0ab8a80ec920795bd6565d2645
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-14 14:34:56 +0200

    Snack6 completed

---
+++ src/org/lessons/java/snacks/Snack6.java
@@ -0,0 +1,26 @@
+package org.lessons.java.snacks;
+
+import java.util.Scanner;
+
+public class Snack6 {
+
+	public static void main(String[] args) {
+	//	Dato un numero sotto forma di stringa, convertirlo in intero senza utilizzare funzioni già pronte.
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Inserisci un numero:" );
+		String stringNum = sc.nextLine();
+		sc.close();
+		
+		int intNum = 0; //INTERO
+		for (int i = 0; i < stringNum.length(); i++) {
+			char charNum = stringNum.charAt(i);
+			int charConverted = charNum - '0'; // 0 vale 48 e fino a 9 il valore aumenta di uno in uno, 
+			//sottraendo quindi ad esempio 1 (49) a 0 (48) avremo il suo valore reale in int (che è appunto 1)
+			 intNum = intNum * 10 + charConverted; //aggiungi uno zero (*10) per la prox iterazione (nella prima 0*0 è 0)
+		}
+		
+		System.out.println(intNum); //STAMPA INTERO
+	}
+
+}
